95cccb878358ef41b2f5f254f089efce849404b6
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-17 14:41:16 -0700

    created function randomNumberGenerator()

--- app.js
+++ app.js
@@ -3,20 +3,32 @@
 function Product(name) {
   this.name = name;
   this.filePath = 'assets/' + name;
-  this.wasDisplayed = 0;
   this.numTimesChosen = 0;
+  this.wasDisplayed = false;
 }
 
-var imageNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+function productsArrayCreate() {
+  var imageNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
-var products = [];
+  var products = [];
 
-for(var i = 0; i < imageNames.length; i++) {
-  console.log(imageNames[i]);
-  products.push(new Product(imageNames[i]));
+  for(var i = 0; i < imageNames.length; i++) {
+    console.log(imageNames[i]);
+    products.push(new Product(imageNames[i]));
+  }
+  return products;
 }
 
+function randomNumberGenerator(max) {
+  return Math.floor(Math.random() * max);
+}
+
+var products = productsArrayCreate();
 var imagesContainer = document.getElementById('images-container');
 var image = document.createElement('img');
-image.setAttribute('src', products[0].filePath);
+
+var random = randomNumberGenerator(products.length);
+console.log(random);
+
+image.setAttribute('src', products[random].filePath);
 imagesContainer.appendChild(image);
