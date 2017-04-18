4ee5b280c1656e5ac59d98768d9bfc842f70060e
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-18 09:05:49 -0700

    table displays correct number of cells

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function tableDataGenerate() {
     td = document.createElement('td');
     td.textContent = products[i].name;
     tr.appendChild(td);
-    for(var j = 0; j < 2; j++) {
+    for(var j = 0; j < 1; j++) {
       td = document.createElement('td');
       td.textContent = products[i].numTimesChosen;
       tr.appendChild(td);
