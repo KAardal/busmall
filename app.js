0f97845dcf46f8a2aec817f7a4381f89382d6d28
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-18 14:29:36 -0700

    rewrote imageDisplay()

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 var clicksRemaining = 5;
 
 function main(){
-  //productsDisplay(productsArrayCreate());
+
+  getImages();
 }
 
-function Product(name) {
+function Product(name, filePath) {
+
   this.name = name;
-  this.filePath = 'assets/' + name;
+  this.filePath = 'assets/' + filePath;
   this.timesChosen = 0;
   this.timesDisplayed = 0;
 }
 
 var products = [
+
   new Product('bag', 'bag.jpg'),
   new Product('banana', 'banana.jpg'),
   new Product('bathroom', 'bathroom.jpg'),
   new Product('boots', 'boots.jpg'),
-  new Product('breakfast', 'breahfast.jpg'),
+  new Product('breakfast', 'breakfast.jpg'),
   new Product('bubblegum', 'bubblegum.jpg'),
   new Product('chair', 'chair.jpg'),
   new Product('cthulhu', 'cthulhu.jpg'),
@@ -36,119 +39,64 @@ var products = [
   new Product('wine-glass', 'wine-glass.jpg'),
 ];
 
+var productsCurrent = [];
+var productsLast = [];
+
 function randomNumberGenerator(max) {
-  return Math.floor(Math.random() * max);
+
+  return Math.floor(Math.random() * max.length);
 }
-//
-// function handleProductClick(event) {
-//   if(clicks < 5){
-//
-//     console.log(event.target.id);
-//     wasDisplayedReset();
-//
-//     var imgOne = document.getElementById('image-one');
-//     imageSourceSet(imgOne);
-//
-//     var imgTwo = document.getElementById('image-two');
-//     imageSourceSet(imgTwo);
-//
-//     var imgThree = document.getElementById('image-three');
-//     imageSourceSet(imgThree);
-//     clicks++;
-//   } else {
-//     tableDataGenerate();
-//   }
-// }
-//
-// function tableDataGenerate() {
-//   var div = document.getElementById('table-container');
-//   var tr;
-//   var td;
-//   var i = 0;
-//   while(i < products.length) {
-//     tr = document.createElement('tr');
-//     td = document.createElement('td');
-//     td.textContent = products[i].name;
-//     tr.appendChild(td);
-//     for(var j = 0; j < 2; j++) {
-//       td = document.createElement('td');
-//       td.textContent = products[i].numTimesChosen;
-//       tr.appendChild(td);
-//     }
-//     div.appendChild(tr);
-//     i++;
-//   }
-// }
-//
-// function productsArrayCreate() {
-//   var imageNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
-//
-//   for(var i = 0; i < imageNames.length; i++) {
-//     console.log(imageNames[i]);
-//     products.push(new Product(imageNames[i]));
-//   }
-// }
-//
-// function imageSourceSet(image) {
-//   var currentProduct;
-//   var i = 0;
-//   while(i < 1) {
-//     var rand = randomNumberGenerator(products.length);
-//     currentProduct = products[rand];
-//     if(!currentProduct.wasDisplayed) {
-//       image.setAttribute('src', currentProduct.filePath);
-//       image.setAttribute('xml:id', rand);
-//       currentProduct.wasDisplayed = true;
-//       image.addEventListener('click', handleProductClick);
-//       i++;
-//     }
-//   }
-//   return image;
-// }
-//
-// function wasDisplayedReset() {
-//   for (var i = 0; i < products.length; i++) {
-//     products[i].wasDisplayed = false;
-//   }
-// }
-//
-// function productsDisplay() {
-//   var imagesContainer = document.getElementById('images-container');
-//
-//   var imagesUl = document.createElement('ul');
-//   imagesUl.setAttribute('id', 'images-ul');
-//
-//   var imagesLi = document.createElement('li');
-//   imagesLi.setAttribute('id', 'images-li');
-//
-//   var imageOne = document.createElement('img');
-//   imageOne.setAttribute('id', 'image-one');
-//
-//   var imageTwo = document.createElement('img');
-//   imageTwo.setAttribute('id', 'image-two');
-//
-//   var imageThree = document.createElement('img');
-//   imageThree.setAttribute('id', 'image-three');
-//
-//   var imagesLiOne = document.createElement('li');
-//   imagesLiOne.appendChild(imageSourceSet(imageOne));
-//   imagesUl.appendChild(imagesLiOne);
-//
-//   var imagesLiTwo = document.createElement('li');
-//   imagesLiTwo.appendChild(imageSourceSet(imageTwo));
-//   imagesUl.appendChild(imagesLiTwo);
-//
-//   var imagesLiThree = document.createElement('li');
-//   imagesLiThree.appendChild(imageSourceSet(imageThree));
-//   imagesUl.appendChild(imagesLiThree);
-//
-//   imagesContainer.appendChild(imagesUl);
-//
-//   //imagesUl.addEventListener('click', handleProductClick);
-// }
-//
-// function randomNumberGenerator(max) {
-//   return Math.floor(Math.random() * max);
-// }
-//
-// main();
+
+function handleProductClick(event) {
+  if(clicksRemaining > 0){
+    console.log(event.target);
+    getImages();
+  }
+}
+
+function getImages() {
+
+  var image = products.splice(randomNumberGenerator(products), 1);
+  productsCurrent = productsCurrent.concat(image);
+  image = products.splice(randomNumberGenerator(products), 1);
+  productsCurrent = productsCurrent.concat(image);
+  image = products.splice(randomNumberGenerator(products), 1);
+  productsCurrent = productsCurrent.concat(image);
+
+  imageDisplay(productsCurrent);
+}
+
+function imageDisplay(list) {
+
+  var imagesContainer = document.getElementById('images-container');
+  var imagesUl = document.createElement('ul');
+  imagesUl.setAttribute('id', 'images-ul');
+
+  var liOne = document.createElement('li');
+  var imageOne = document.createElement('img');
+  imageOne.setAttribute('id', 'img-1');
+  imageOne.setAttribute('src', list[0].filePath);
+  imageOne.setAttribute('alt', list[0].name);
+  liOne.appendChild(imageOne);
+  imagesUl.appendChild(liOne);
+
+  var liTwo = document.createElement('li');
+  var imageTwo = document.createElement('img');
+  imageTwo.setAttribute('id', 'img-2');
+  imageTwo.setAttribute('src', list[1].filePath);
+  imageTwo.setAttribute('alt', list[1].name);
+  liTwo.appendChild(imageTwo);
+  imagesUl.appendChild(liTwo);
+
+  var liThree = document.createElement('li');
+  var imageThree = document.createElement('img');
+  imageThree.setAttribute('id', 'img-3');
+  imageThree.setAttribute('src', list[2].filePath);
+  imageThree.setAttribute('alt', list[2].name);
+  liThree.appendChild(imageThree);
+  imagesUl.appendChild(liThree);
+
+  imagesContainer.appendChild(imagesUl);
+}
+
+main();
