cbdd8275ad53428d70c34114986ac3a9359f40ae
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-18 14:59:41 -0700

    program is now working up to yesterdays requirements... i think

--- app.js
+++ app.js
@@ -49,12 +49,23 @@ function randomNumberGenerator(max) {
 
 function handleProductClick(event) {
   if(clicksRemaining > 0){
-    console.log(event.target);
+    productsCurrent[parseInt(event.target.id)].timesChosen++;
+    var el = document.getElementById('images-container');
+    el.textContent = '';
+    clicksRemaining--;
     getImages();
+  } else {
+    products = products.concat(productsCurrent);
+    products = products.concat(productsLast);
+    console.log(products);
   }
 }
 
 function getImages() {
+
+  products = products.concat(productsLast);
+  productsLast = productsCurrent;
+  productsCurrent = [];
 
   var image = products.splice(randomNumberGenerator(products), 1);
   productsCurrent = productsCurrent.concat(image);
@@ -72,31 +83,29 @@ function imageDisplay(list) {
   var imagesUl = document.createElement('ul');
   imagesUl.setAttribute('id', 'images-ul');
 
-  var liOne = document.createElement('li');
-  var imageOne = document.createElement('img');
-  imageOne.setAttribute('id', 'img-1');
-  imageOne.setAttribute('src', list[0].filePath);
-  imageOne.setAttribute('alt', list[0].name);
-  liOne.appendChild(imageOne);
-  imagesUl.appendChild(liOne);
+  var imagesLi;
+  var image;
+  for(var i = 0; i < list.length; i++) {
 
-  var liTwo = document.createElement('li');
-  var imageTwo = document.createElement('img');
-  imageTwo.setAttribute('id', 'img-2');
-  imageTwo.setAttribute('src', list[1].filePath);
-  imageTwo.setAttribute('alt', list[1].name);
-  liTwo.appendChild(imageTwo);
-  imagesUl.appendChild(liTwo);
+    imagesLi = document.createElement('li');
+    image = setSource(list, i);
+    imagesLi.addEventListener('click', handleProductClick);
 
-  var liThree = document.createElement('li');
-  var imageThree = document.createElement('img');
-  imageThree.setAttribute('id', 'img-3');
-  imageThree.setAttribute('src', list[2].filePath);
-  imageThree.setAttribute('alt', list[2].name);
-  liThree.appendChild(imageThree);
-  imagesUl.appendChild(liThree);
+    imagesLi.appendChild(image);
+    imagesUl.appendChild(imagesLi);
+    productsCurrent[i].timesDisplayed++;
+  }
 
   imagesContainer.appendChild(imagesUl);
+}
+
+function setSource(list, num){
+
+  var image = document.createElement('img');
+  image.setAttribute('id', num);
+  image.setAttribute('src', list[num].filePath);
+  image.setAttribute('alt', list[num].name);
+  return image;
 }
 
 main();
