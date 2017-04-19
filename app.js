9abc54d1316d83cecaf62440bd9e9d23fcc2b280
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-18 18:18:43 -0700

    fixed white spacing

--- app.js
+++ app.js
@@ -50,12 +50,16 @@ function randomNumberGenerator(max) {
 function handleProductClick(event) {
 
   if(clicksRemaining > 0){
+
     productsCurrent[parseInt(event.target.id)].timesChosen++;
+
     var el = document.getElementById('images-container');
     el.textContent = '';
+
     clicksRemaining--;
     getImages();
   } else {
+
     products = products.concat(productsCurrent);
     products = products.concat(productsLast);
     getResults();
@@ -70,8 +74,10 @@ function getImages() {
 
   var image = products.splice(randomNumberGenerator(products), 1);
   productsCurrent = productsCurrent.concat(image);
+
   image = products.splice(randomNumberGenerator(products), 1);
   productsCurrent = productsCurrent.concat(image);
+
   image = products.splice(randomNumberGenerator(products), 1);
   productsCurrent = productsCurrent.concat(image);
 
@@ -112,28 +118,32 @@ function setSource(list, num){
 function getResults() {
 
   var ctx = document.getElementById('results-chart');
-
   var resultsChart = new Chart(ctx, {
 
     type: 'bar',
+
     data: {
       labels: [products[0].name, products[1].name, products[2].name, products[3].name, products[4].name, products[5].name, products[6].name, products[7].name, products[8].name, products[9].name, products[10].name, products[11].name, products[12].name, products[13].name, products[14].name, products[15].name, products[16].name, products[17].name, products[18].name, products[19].name,],
 
       datasets: [{
         label: '# of Votes',
+
         data: [products[0].timesChosen, products[1].timesChosen, products[2].timesChosen, products[3].timesChosen, products[4].timesChosen, products[5].timesChosen, products[6].timesChosen, products[7].timesChosen, products[8].timesChosen, products[9].timesChosen, products[10].timesChosen, products[11].timesChosen, products[12].timesChosen, products[13].timesChosen, products[14].timesChosen, products[15].timesChosen, products[16].timesChosen, products[17].timesChosen, products[18].timesChosen, products[19].timesChosen,],
+
         backgroundColor: [
           '#FFDBE5', '#7A4900', '#0000A6', '#63FFAC', '#B79762', '#004D43', '#8FB0FF', '#997D87',
           '#5A0007', '#809693', '#FEFFE6', '#1B4400', '#4FC601', '#3B5DFF', '#4A3B53', '#FF2F80',
           '#61615A', '#BA0900', '#6B7900', '#00C2A0', '#FFAA92',
         ],
-        borderColor: [
 
-        ],
-        borderWidth: 1
+        borderColor: [],
+
+        borderWidth: 1,
       },
+
       {
         label: '% of votes',
+
         data: [
           (products[0].timesChosen / products[0].timesDisplayed) * 100,
           (products[1].timesChosen / products[1].timesDisplayed) * 100,
@@ -158,15 +168,13 @@ function getResults() {
           (products[17].timesChosen / products[17].timesDisplayed) * 100,
           (products[18].timesChosen / products[18].timesDisplayed) * 100,
           (products[19].timesChosen / products[19].timesDisplayed) * 100,],
-        backgroundColor: [
-          '#FFDBE5', '#7A4900', '#0000A6', '#63FFAC', '#B79762', '#004D43', '#8FB0FF', '#997D87',
-          '#5A0007', '#809693', '#FEFFE6', '#1B4400', '#4FC601', '#3B5DFF', '#4A3B53', '#FF2F80',
-          '#61615A', '#BA0900', '#6B7900', '#00C2A0', '#FFAA92',
-        ],
-        borderColor: [
 
-        ],
-        borderWidth: 1
+        backgroundColor: [
+          '#FFDBE5', '#7A4900', '#0000A6', '#63FFAC', '#B79762', '#004D43', '#8FB0FF', '#997D87', '#5A0007', '#809693', '#FEFFE6', '#1B4400', '#4FC601', '#3B5DFF', '#4A3B53', '#FF2F80', '#61615A', '#BA0900', '#6B7900', '#00C2A0', '#FFAA92'],
+
+        borderColor: [],
+
+        borderWidth: 1,
       }]
     },
     options: {
